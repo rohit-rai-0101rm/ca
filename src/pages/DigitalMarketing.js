553957d16cdbaf43224b2ce1450b63decdaa7bd1
rohit-rai-0101rm <rohitrai.dev@gmail.com>
2022-12-03 15:42:57 +0530

    03m dec 1542

--- src/pages/DigitalMarketing.js
+++ src/pages/DigitalMarketing.js
@@ -1,8 +1,30 @@
 import React from 'react'
+import FooterOne from '../common/footer/FooterOne'
+import HeaderOne from '../common/header/HeaderOne'
+import SEO from '../common/SEO'
+import AboutFive from '../component/about/AboutFive'
+import AboutFour from '../component/about/AboutFour'
+import AboutThree from '../component/about/AboutThree'
+import CtaLayoutOne from '../component/cta/CtaLayoutOne'
+import ProcessOne from '../component/process/ProcessOne'
+import ColorSwitcher from '../elements/switcher/ColorSwitcher'
 
 const DigitalMarketing = () => {
   return (
-    <div>DigitalMarketing</div>
+    <>
+    <SEO title="About us" />
+    <ColorSwitcher />
+    <main className="main-wrapper">
+      <HeaderOne />
+     
+      <AboutFour />
+      <AboutThree />
+      <AboutFive />
+      <ProcessOne />
+      <CtaLayoutOne />
+      <FooterOne parentClass="" />
+    </main>
+  </>
   )
 }
 
