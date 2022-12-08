ad5ed31bb9b98106d20374287895e4cf3854b678
Bhupender2 <b.mehtaa2021@gmail.com>
2022-12-08 12:49:35 +0530

    manage service provider

--- src/pages/ManageServiceProvider.js
+++ src/pages/ManageServiceProvider.js
@@ -11,6 +11,7 @@ import CaseStudyProp from "../component/casestudy/CaseStudyProp";
 import Content from "../component/content/Content";
 import CounterUpOne from "../component/counterup/CounterUpOne";
 import CtaLayoutOne from "../component/cta/CtaLayoutOne";
+import DifferentServices from "../component/DifferentServices/DifferentServices";
 import Innovate from "../component/Innovate/Innovate";
 import PricingOne from "../component/pricing/PricingOne";
 import ProjectOne from "../component/project/ProjectOne";
@@ -29,81 +30,10 @@ const ManageServiceProvider = () => {
       <ColorSwitcher />
       <main className="main-wrapper">
       <Provider />
-        <HeaderOne />
-        <BannerOne />
-
-        <BrandOne />
-        <div className="section bg-color-light section-padding">
-          <div className="container">
-            <h2 className="text2center">
-              creating something truly
-             
-              <strong> unique!</strong>
-            </h2>
-            <PricingOne />
-          </div>
-          <ul className="list-unstyled shape-group-3">
-            <li className="shape shape-1">
-              <img
-                src={process.env.PUBLIC_URL + "/images/others/line-1.png"}
-                alt="shape"
-              />
-            </li>
-            <li className="shape shape-2">
-              <img
-                src={process.env.PUBLIC_URL + "/images/others/bubble-4.png"}
-                alt="shape"
-              />
-            </li>
-          </ul>
-        </div>
-
-        <div className="section section-padding-2 bg-color-dark">
-          <div className="container">
-            <SectionTitle
-              subtitle="What We Can Do For You"
-              title="services that get you results"
-              description="Transform how your business works. We brainstorm together, develop strategies & build brands that guarantee business."
-              textAlignment="heading-light-left"
-              textColor=""
-            />
-            <div className="row">
-              <ServicePropOne
-                colSize="col-xl-4 col-md-6"
-                serviceStyle=""
-                itemShow="18"
-              />
-            </div>
-          </div>
-          <ul className="list-unstyled shape-group-10">
-            <li className="shape shape-1">
-              <img
-                src={process.env.PUBLIC_URL + "/images/others/line-9.png"}
-                alt="Circle"
-              />
-            </li>
-            <li className="shape shape-2">
-              <img
-                src={process.env.PUBLIC_URL + "/images/others/bubble-42.png"}
-                alt="Circle"
-              />
-            </li>
-            <li className="shape shape-3">
-              <img
-                src={process.env.PUBLIC_URL + "/images/others/bubble-43.png"}
-                alt="Circle"
-              />
-            </li>
-          </ul>
-        </div>
-        
-        <Innovate />
-        <Content />
-        <CounterUpOne />
-        <TeamOne />
-        <CtaLayoutOne />
-
-        <FooterOne parentClass="" />
+      <DifferentServices/>
+      <HeaderOne />
+      <CtaLayoutOne />
+      <FooterOne parentClass="" />
       </main>
     </>
   );
