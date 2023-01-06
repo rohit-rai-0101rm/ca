dade32e407aa006a601ff0341339ae87951663d2
Rohit Rai <rohitrai.dev@gmail.com>
2023-01-06 17:07:35 +0530

    06 jan 1707

--- src/common/header/Nav.js
+++ src/common/header/Nav.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FaAngleDown, FaAngleRight } from "react-icons/fa";
+import Dropdown from 'react-multilevel-dropdown';
 
 
 const Nav = () => {
@@ -22,55 +23,108 @@ const Nav = () => {
 
                     </ul>
                 </li>
+                <li className='menu-item-has-children'>
+                    <Dropdown
 
-                <li className="menu-item-has-children2">
-                    <Link to="#">Hire Top Talent<FaAngleDown /></Link>
-                    <div>
-                        <ul className="axil-submenu2">
+                        openOnHover="true"
+                        title='Dedicated Resources'
 
+                    >
+                        <Dropdown.Item className='axil-submenu2'>
+                            Digital Marketers <FaAngleRight />
+                            <Dropdown.Submenu position="right">
+                                <Link to="/hire-ppc-expert">
+                                    <Dropdown.Item>
+                                        SEM && PPC specialist
+                                    </Dropdown.Item>
+                                </Link>
 
+                                <Dropdown.Item>
+                                    SEO Specialist                            </Dropdown.Item>
+                                <Dropdown.Item>
+                                    Programmatic Specialist                            </Dropdown.Item>
+                            </Dropdown.Submenu>
+                        </Dropdown.Item>
+                        <Dropdown.Item>
+                            Web Developers   <FaAngleRight />                        <Dropdown.Submenu position="right">
+                                <Dropdown.Item>
+                                    React JS
+                                </Dropdown.Item>
+                                <Dropdown.Item>
+                                    Angular JS
+                                </Dropdown.Item>
+                                <Dropdown.Item>
+                                    Node JS
+                                </Dropdown.Item>
+                                <Dropdown.Item>
+                                    Laravel JS                            </Dropdown.Item>
+                                <Dropdown.Item>
+                                    Shopify        Developer                    </Dropdown.Item>
+                                <Dropdown.Item>
+                                    PHP        Developer
+                                </Dropdown.Item>
+                                <Dropdown.Item>
+                                    Wordpress Developer                            </Dropdown.Item>
+                                <Dropdown.Item>
+                                    Wordpress Plugin Developer                              </Dropdown.Item>
+                                <Dropdown.Item>
+                                    Wordpress Designer                             </Dropdown.Item>
 
-
-
-                            <li>Digital Marketers <FaAngleRight />
-
-                                <ul className="axil-submenu3">
-                                    <li><Link to={process.env.PUBLIC_URL + "/about"}>About</Link></li>
-                                    <li><Link to="/how-it-works">How it works</Link></li>
-                                    <li><Link to="/why-india" >Why India</Link></li>
-                                    <li><Link>Pricing</Link></li>
-
-                                </ul>
-
-                            </li>
-
-                            <li>Web Developers <FaAngleRight /> 
-                            
-                            <ul className="axil-submenu4">
-                                    <li><Link to={process.env.PUBLIC_URL + "/about"}>About2</Link></li>
-                                    <li><Link to="/how-it-works">How it works2</Link></li>
-                                    <li><Link to="/why-india" >Why India2</Link></li>
-                                    <li><Link>Pricing2</Link></li>
-
-                                </ul>
-
-                            
-                            </li>
-                            <li>Designers <FaAngleRight /> </li>
-
-                            <li>Salesforce Expert <FaAngleRight /> </li>
-                            <li>Software Developers<FaAngleRight /> </li>
-                            <li>Mobile App Developers<FaAngleRight /> </li>
-
-                        </ul>
-                    </div>
-
-
-
-
-
-
+                            </Dropdown.Submenu>
+                        </Dropdown.Item>
+                        <Dropdown.Item>
+                            Designers     <FaAngleRight />                     <Dropdown.Submenu position="right">
+                                <Dropdown.Item>
+                                    Graphics Designers                            </Dropdown.Item>
+                                <Dropdown.Item>
+                                    UX/UI Designer                           </Dropdown.Item>
+                            </Dropdown.Submenu>
+                        </Dropdown.Item>
+                        <Dropdown.Item>
+                            Salesforce Expert  <FaAngleRight />                        <Dropdown.Submenu position="right">
+                                <Dropdown.Item>
+                                    Salesforce Developer
+                                </Dropdown.Item>
+                                <Dropdown.Item>
+                                    Salesforce Administrator
+                                </Dropdown.Item>
+                                <Dropdown.Item>
+                                    Salesforce Consultant
+                                </Dropdown.Item>
+                            </Dropdown.Submenu>
+                        </Dropdown.Item>
+                        <Dropdown.Item>
+                            Software Developers <FaAngleRight />                       <Dropdown.Submenu position="right">
+                                <Dropdown.Item>
+                                    Java Developer                            </Dropdown.Item>
+                                <Dropdown.Item>
+                                    Python Developer                            </Dropdown.Item>
+                                <Dropdown.Item>
+                                    .NET Developer                            </Dropdown.Item>
+                            </Dropdown.Submenu>
+                        </Dropdown.Item>
+                        <Dropdown.Item>
+                            Mobile App Developers  <FaAngleRight />                         <Dropdown.Submenu position="right">
+                                <Dropdown.Item>
+                                    ios Developer
+                                </Dropdown.Item>
+                                <Dropdown.Item>
+                                    Android Developer
+                                </Dropdown.Item>
+                                <Dropdown.Item>
+                                    React Native Developer
+                                </Dropdown.Item>
+                            </Dropdown.Submenu>
+                        </Dropdown.Item>
+                    </Dropdown>
                 </li>
+
+
+
+
+
+
+
 
 
                 <li className="menu-item-has-children">
