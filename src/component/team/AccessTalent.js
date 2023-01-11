01a1a401ce644cf2e9bee2cb0b7164afa55d8d6c
bhupender2 <b.mehtaa2021@gmail.com>
2023-01-11 16:42:19 +0530

    kk

--- src/component/team/AccessTalent.js
+++ src/component/team/AccessTalent.js
@@ -5,79 +5,43 @@ import { slugify } from "../../utils";
 
 const allData = TeamData;
 
-const AccessTalentPython = () => {
+const AccessTalent = () => {
   return (
-    <div className="section section-padding bg-color-dark pb--70 pb_lg--20 pb_md--0">
+    <div className="section section-padding  pb--70 pb_lg--20 pb_md--0">
       <div className="container">
         <div className="section-heading heading-light">
-          <h3 className="title mt--100">
-          Land India’s top remote talent with Dignifyd
-
-          </h3>
-          <p className="innovate-list-para">Access the talent pool of 50,000+ skilled professionals with knowledge of more than 20 tech stacks
-
-</p>
+          <h3 className="title mb--50 skillsHeading">Land India’s top remote engineers with Dignifyd</h3>
+          <p className="innovate-list-para">Access the talent pool of 50,000+ skilled professionals with knowledge of more than 20 tech stacks</p>
         </div>
         <div className="row">
           {allData.map((data) => (
             <div className="col-xl-3 col-sm-6" key={data.id}>
               <div className="team-grid">
                 <div className="thumbnail">
-                  <Link
-                    to={
-                      process.env.PUBLIC_URL +
-                      `/team-details/${slugify(data.title)}`
-                    }
-                  >
+             
                     <img
                       src={process.env.PUBLIC_URL + data.thumb}
                       alt={data.title}
                     />
-                  </Link>
                 </div>
                 <div className="content">
-                  <h4 className="title">
-                    <Link
-                      to={
-                        process.env.PUBLIC_URL +
-                        `/team-details/${slugify(data.title)}`
-                      }
-                    >
-                      {data.title}
-                    </Link>
-                  </h4>
-                  <span
-                    className="designation"
-                    dangerouslySetInnerHTML={{ __html: data.designation }}
-                  ></span>
+              
+                 <p className="innovate-list-para">{data.designation}</p>
                 </div>
               </div>
             </div>
           ))}
         </div>
       </div>
-      <ul className="list-unstyled shape-group-10">
-        <li className="shape shape-1">
-          <img
-            src={process.env.PUBLIC_URL + "/images/others/circle-1.png"}
-            alt="Circle"
-          />
-        </li>
-        <li className="shape shape-2">
-          <img
-            src={process.env.PUBLIC_URL + "/images/others/line-3.png"}
-            alt="Circle"
-          />
-        </li>
-        <li className="shape shape-3">
-          <img
-            src={process.env.PUBLIC_URL + "/images/others/bubble-5.png"}
-            alt="Circle"
-          />
-        </li>
-      </ul>
+     <center> <Link
+              to={process.env.PUBLIC_URL + "/contact"}
+              className="axil-btn btn-large btn-fill-white"
+            >
+              Get in Touch
+            </Link></center> 
+    
     </div>
   );
 };
 
-export default AccessTalentPython;
+export default AccessTalent;
