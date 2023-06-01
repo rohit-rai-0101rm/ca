a60b079bf1e09198d7124681556bc8f37d34921e
lily-002 <lilysharma002@gmail.com>
2023-06-01 11:18:42 +0530

    nxg new website

--- src/App.js
+++ src/App.js
@@ -100,117 +100,117 @@ import Startup from './pages/Startup';
 
 
 const App = () => {
-  return (
-    <Router>
-		<ScrollToTop>
-			<Routes>
-				<Route path={process.env.PUBLIC_URL + "/"} element={<DigitalAgency />}/>
-				<Route path={process.env.PUBLIC_URL + "/how-it-works"} element={<HowWorks />}/>
+	return (
+		<Router>
+			<ScrollToTop>
+				<Routes>
+					<Route path={process.env.PUBLIC_URL + "/"} element={<DigitalAgency />}/>
+					<Route path={process.env.PUBLIC_URL + "/how-it-works"} element={<HowWorks />}/>
 
 
-				<Route path={process.env.PUBLIC_URL + "/digital-agency"} element={<DigitalAgency />}/>
-				<Route path={process.env.PUBLIC_URL + "/creative-agency"} element={<CreativeAgency />}/>
-				<Route path={process.env.PUBLIC_URL + "/personal-portfolio"} element={<PersonalPortfolio />}/>
-				<Route path={process.env.PUBLIC_URL + "/home-startup"} element={<HomeStartup />}/>
-				<Route path={process.env.PUBLIC_URL + "/corporate-agency"} element={<CorporateAgency />}/>
+					<Route path={process.env.PUBLIC_URL + "/digital-agency"} element={<DigitalAgency />}/>
+					<Route path={process.env.PUBLIC_URL + "/creative-agency"} element={<CreativeAgency />}/>
+					<Route path={process.env.PUBLIC_URL + "/personal-portfolio"} element={<PersonalPortfolio />}/>
+					<Route path={process.env.PUBLIC_URL + "/home-startup"} element={<HomeStartup />}/>
+					<Route path={process.env.PUBLIC_URL + "/corporate-agency"} element={<CorporateAgency />}/>
 
-				{/* Blogs */}
-				<Route path={process.env.PUBLIC_URL + "/blog-grid/"} element={<BlogGridView />}/>
-				<Route path={process.env.PUBLIC_URL + "/category/:slug"} element={<BlogCategory />}/>
-				<Route path={process.env.PUBLIC_URL + "/archive/:slug"} element={<BlogArchive />}/>
-				<Route path={process.env.PUBLIC_URL + "/blog-details/:id"} element={<BlogDetails />}/>
-				<Route path={process.env.PUBLIC_URL + "/services/seo"} element={<SeoPage />}/>
+					{/* Blogs */}
+					<Route path={process.env.PUBLIC_URL + "/blog-grid/"} element={<BlogGridView />}/>
+					<Route path={process.env.PUBLIC_URL + "/category/:slug"} element={<BlogCategory />}/>
+					<Route path={process.env.PUBLIC_URL + "/archive/:slug"} element={<BlogArchive />}/>
+					<Route path={process.env.PUBLIC_URL + "/blog-details/:id"} element={<BlogDetails />}/>
+					<Route path={process.env.PUBLIC_URL + "/services/seo"} element={<SeoPage />}/>
 
-				{/* Service */}
-				<Route path={process.env.PUBLIC_URL + "/service-one/"} element={<ServiceOne />}/>
-				<Route path={process.env.PUBLIC_URL + "/service-two/"} element={<ServiceTwo />}/>
-				<Route path={process.env.PUBLIC_URL + "/service-details/:slug"} element={<ServiceDetails />}/>
+					{/* Service */}
+					<Route path={process.env.PUBLIC_URL + "/service-one/"} element={<ServiceOne />}/>
+					<Route path={process.env.PUBLIC_URL + "/service-two/"} element={<ServiceTwo />}/>
+					<Route path={process.env.PUBLIC_URL + "/service-details/:slug"} element={<ServiceDetails />}/>
 
-				{/* Project  */}
-				<Route path={process.env.PUBLIC_URL + "/project-grid-one"} element={<ProjectGridOne />}/>
-				<Route path={process.env.PUBLIC_URL + "/project-grid-two"} element={<ProjectGridTwo />}/>
-				<Route path={process.env.PUBLIC_URL + "/project-grid-three"} element={<ProjectGridThree />}/>
-				<Route path={process.env.PUBLIC_URL + "/project-width-one"} element={<ProjectGridFour />}/>
-				<Route path={process.env.PUBLIC_URL + "/project-width-two"} element={<ProjectGridFive />}/>
-				<Route path={process.env.PUBLIC_URL + "/project-details/:slug"} element={<ProjectDetails />}/>
-				<Route path={process.env.PUBLIC_URL + "/about"} element={<WhyAbout />}/>
+					{/* Project  */}
+					<Route path={process.env.PUBLIC_URL + "/project-grid-one"} element={<ProjectGridOne />}/>
+					<Route path={process.env.PUBLIC_URL + "/project-grid-two"} element={<ProjectGridTwo />}/>
+					<Route path={process.env.PUBLIC_URL + "/project-grid-three"} element={<ProjectGridThree />}/>
+					<Route path={process.env.PUBLIC_URL + "/project-width-one"} element={<ProjectGridFour />}/>
+					<Route path={process.env.PUBLIC_URL + "/project-width-two"} element={<ProjectGridFive />}/>
+					<Route path={process.env.PUBLIC_URL + "/project-details/:slug"} element={<ProjectDetails />}/>
+					<Route path={process.env.PUBLIC_URL + "/about"} element={<WhyAbout />}/>
 
-				{/* Pages  */}
-				<Route path={process.env.PUBLIC_URL + "/about-us"} element={<AboutUs />}/>
-				<Route path={process.env.PUBLIC_URL + "/our-office"} element={<OurOffice />}/>
-				<Route path={process.env.PUBLIC_URL + "/our-clients"} element={<OurClients />}/>
-				<Route path={process.env.PUBLIC_URL + "/team"} element={<Team />}/>
-				<Route path={process.env.PUBLIC_URL + "/team-details/:slug"} element={<TeamDetails />}/>
-				<Route path={process.env.PUBLIC_URL + "/case-study"} element={<CaseStudy />}/>
-				<Route path={process.env.PUBLIC_URL + "/case-details/:slug"} element={<CaseDetails />}/>
-				<Route path={process.env.PUBLIC_URL + "/testimonials"} element={<Testimonials />}/>
-				<Route path={process.env.PUBLIC_URL + "/pricing-table"} element={<PricingTable />}/>
-				<Route path={process.env.PUBLIC_URL + "/typography"} element={<Typography />}/>
-				<Route path={process.env.PUBLIC_URL + "/contact"} element={<Contact />}/>
-				<Route path={process.env.PUBLIC_URL + "/404"} element={<ErrorPage />}/>
-				<Route path={process.env.PUBLIC_URL + "/coming-soon"} element={<ComingSoon />}/>
-				<Route path={process.env.PUBLIC_URL + "/privacy-policy"} element={<PrivacyPolicy />}/>
-				<Route path={process.env.PUBLIC_URL + "/terms-use"} element={<TermsOfUse />}/>
-				<Route path={process.env.PUBLIC_URL + "/remote-staffing"} element={<RemoteStaffing />}/>
-				<Route path={process.env.PUBLIC_URL + "/banking-fintech"} element={<Banking/>}/>
-				<Route path={process.env.PUBLIC_URL + "/pharma"} element={<Pharma/>}/>
-				<Route path={process.env.PUBLIC_URL + "/startup"} element={<Startup/>}/>
-				
+					{/* Pages  */}
+					<Route path={process.env.PUBLIC_URL + "/about-us"} element={<AboutUs />}/>
+					<Route path={process.env.PUBLIC_URL + "/our-office"} element={<OurOffice />}/>
+					<Route path={process.env.PUBLIC_URL + "/our-clients"} element={<OurClients />}/>
+					<Route path={process.env.PUBLIC_URL + "/team"} element={<Team />}/>
+					<Route path={process.env.PUBLIC_URL + "/team-details/:slug"} element={<TeamDetails />}/>
+					<Route path={process.env.PUBLIC_URL + "/case-study"} element={<CaseStudy />}/>
+					<Route path={process.env.PUBLIC_URL + "/case-details/:slug"} element={<CaseDetails />}/>
+					<Route path={process.env.PUBLIC_URL + "/testimonials"} element={<Testimonials />}/>
+					<Route path={process.env.PUBLIC_URL + "/pricing-table"} element={<PricingTable />}/>
+					<Route path={process.env.PUBLIC_URL + "/typography"} element={<Typography />}/>
+					<Route path={process.env.PUBLIC_URL + "/contact"} element={<Contact />}/>
+					<Route path={process.env.PUBLIC_URL + "/404"} element={<ErrorPage />}/>
+					<Route path={process.env.PUBLIC_URL + "/coming-soon"} element={<ComingSoon />}/>
+					<Route path={process.env.PUBLIC_URL + "/privacy-policy"} element={<PrivacyPolicy />}/>
+					<Route path={process.env.PUBLIC_URL + "/terms-use"} element={<TermsOfUse />}/>
+					<Route path={process.env.PUBLIC_URL + "/remote-staffing"} element={<RemoteStaffing />}/>
+					<Route path={process.env.PUBLIC_URL + "/banking-fintech"} element={<Banking/>}/>
+					<Route path={process.env.PUBLIC_URL + "/pharma"} element={<Pharma/>}/>
+					<Route path={process.env.PUBLIC_URL + "/startup"} element={<Startup/>}/>
+					
 
-				<Route path={process.env.PUBLIC_URL + "/services/seo"} element={<Seo />}/>
-				<Route path={process.env.PUBLIC_URL + "/services/digital-marketing"} element={<DigitalMarketing />}/>
-
-
-				<Route path={process.env.PUBLIC_URL + "/services/web-development"} element={<Webdevelopment />}/>
-				<Route path={process.env.PUBLIC_URL + "/services/design"} element={<Design />}/>
-				<Route path={process.env.PUBLIC_URL + "/why-india"} element={<WhyIndia />}/>
-				<Route path={process.env.PUBLIC_URL + "/dedicated-resources"} element={<DedicatedResource />}/>
-
-				
-				
-
-				<Route path={process.env.PUBLIC_URL + "/managed-service-provider"} element={<ManageServiceProvider />}/>
-                <Route path={process.env.PUBLIC_URL + "/dedicated-resources"} element={<DedicatedResources />}/>
-                <Route path={process.env.PUBLIC_URL + "/hire-reactjs-developers"} element={<ReactDeveloper/>}/>
-				<Route path={process.env.PUBLIC_URL + "/hire-angularjs-developers"} element={<AngularDevelopers/>}/>
-				<Route path={process.env.PUBLIC_URL + "/hire-nodejs-developers"} element={<NodeDevelopers/>}/>
-				<Route path={process.env.PUBLIC_URL + "/hire-wordpress-developers"} element={<WordPressDeveloper/>}/>
-				<Route path={process.env.PUBLIC_URL + "/hire-laravel-developers"} element={<LaravelDevelopers/>}/>
-				<Route path={process.env.PUBLIC_URL + "/hire-seo-expert"} element={<SeoExperts/>}/>
-				<Route path={process.env.PUBLIC_URL + "/hire-ppc-expert"} element={<PpcExperts/>}/>
-				<Route path={process.env.PUBLIC_URL + "/automobile"} element={<Automobile/>}/>
+					<Route path={process.env.PUBLIC_URL + "/services/seo"} element={<Seo />}/>
+					<Route path={process.env.PUBLIC_URL + "/services/digital-marketing"} element={<DigitalMarketing />}/>
 
 
-				<Route path={process.env.PUBLIC_URL + "/hire-php-developers"} element={<PHPDevelopers/>}/>
-				<Route path={process.env.PUBLIC_URL + "/hire-wordpress-plugin-developer"} element={<PluginDevelopers/>}/>
-				<Route path={process.env.PUBLIC_URL + "/hire-wordpress-designer"} element={<WordPressDesigner/>}/>
-				<Route path={process.env.PUBLIC_URL + "/hire-shopify-developers"} element={<ShopifyDevelopers/>}/>
-				<Route path={process.env.PUBLIC_URL + "/hire-graphic-designers"} element={<GraphicDesigner/>}/>
-				<Route path={process.env.PUBLIC_URL + "/hire-ui-ux-designers"} element={<UiuxDesigner/>}/>
-				<Route path={process.env.PUBLIC_URL + "/hire-python-developers"} element={<PythonDevelopoers/>}/>
-				<Route path={process.env.PUBLIC_URL + "/hire-salesforce-developer"} element={<SalesDeveloper/>}/>
-				<Route path={process.env.PUBLIC_URL + "/hire-salesforce-administrator"} element={<SalesAdministrator/>}/>
-				<Route path={process.env.PUBLIC_URL + "/hire-salesforce-consultant"} element={<SalesConsultant/>}/>
-				<Route path={process.env.PUBLIC_URL + "/hire-java-developers"} element={<JavaDevelopers/>}/>
-				<Route path={process.env.PUBLIC_URL + "/hire-ios-developers"} element={<IosDeveloper/>}/>
-				<Route path={process.env.PUBLIC_URL + "/hire-android-developers"} element={<AndroidDevelopers/>}/>
+					<Route path={process.env.PUBLIC_URL + "/services/web-development"} element={<Webdevelopment />}/>
+					<Route path={process.env.PUBLIC_URL + "/services/design"} element={<Design />}/>
+					<Route path={process.env.PUBLIC_URL + "/why-india"} element={<WhyIndia />}/>
+					<Route path={process.env.PUBLIC_URL + "/dedicated-resources"} element={<DedicatedResource />}/>
+
+					
+					
+
+					<Route path={process.env.PUBLIC_URL + "/managed-service-provider"} element={<ManageServiceProvider />}/>
+					<Route path={process.env.PUBLIC_URL + "/dedicated-resources"} element={<DedicatedResources />}/>
+					<Route path={process.env.PUBLIC_URL + "/hire-reactjs-developers"} element={<ReactDeveloper/>}/>
+					<Route path={process.env.PUBLIC_URL + "/hire-angularjs-developers"} element={<AngularDevelopers/>}/>
+					<Route path={process.env.PUBLIC_URL + "/hire-nodejs-developers"} element={<NodeDevelopers/>}/>
+					<Route path={process.env.PUBLIC_URL + "/hire-wordpress-developers"} element={<WordPressDeveloper/>}/>
+					<Route path={process.env.PUBLIC_URL + "/hire-laravel-developers"} element={<LaravelDevelopers/>}/>
+					<Route path={process.env.PUBLIC_URL + "/hire-seo-expert"} element={<SeoExperts/>}/>
+					<Route path={process.env.PUBLIC_URL + "/hire-ppc-expert"} element={<PpcExperts/>}/>
+					<Route path={process.env.PUBLIC_URL + "/automobile"} element={<Automobile/>}/>
 
 
-				
+					<Route path={process.env.PUBLIC_URL + "/hire-php-developers"} element={<PHPDevelopers/>}/>
+					<Route path={process.env.PUBLIC_URL + "/hire-wordpress-plugin-developer"} element={<PluginDevelopers/>}/>
+					<Route path={process.env.PUBLIC_URL + "/hire-wordpress-designer"} element={<WordPressDesigner/>}/>
+					<Route path={process.env.PUBLIC_URL + "/hire-shopify-developers"} element={<ShopifyDevelopers/>}/>
+					<Route path={process.env.PUBLIC_URL + "/hire-graphic-designers"} element={<GraphicDesigner/>}/>
+					<Route path={process.env.PUBLIC_URL + "/hire-ui-ux-designers"} element={<UiuxDesigner/>}/>
+					<Route path={process.env.PUBLIC_URL + "/hire-python-developers"} element={<PythonDevelopoers/>}/>
+					<Route path={process.env.PUBLIC_URL + "/hire-salesforce-developer"} element={<SalesDeveloper/>}/>
+					<Route path={process.env.PUBLIC_URL + "/hire-salesforce-administrator"} element={<SalesAdministrator/>}/>
+					<Route path={process.env.PUBLIC_URL + "/hire-salesforce-consultant"} element={<SalesConsultant/>}/>
+					<Route path={process.env.PUBLIC_URL + "/hire-java-developers"} element={<JavaDevelopers/>}/>
+					<Route path={process.env.PUBLIC_URL + "/hire-ios-developers"} element={<IosDeveloper/>}/>
+					<Route path={process.env.PUBLIC_URL + "/hire-android-developers"} element={<AndroidDevelopers/>}/>
 
-				<Route path={process.env.PUBLIC_URL + "/hire-dot-net-developers"} element={<DotNetDevelopers/>}/>
 
-				<Route path={process.env.PUBLIC_URL + "/hire-react-native-developers"} element={<ReactNativeDevelopers/>}/>
+					
 
-				<Route path={process.env.PUBLIC_URL + "/programmatic-advertising"} element={<ProgrammaticSpecialist/>}/>
-				<Route path={process.env.PUBLIC_URL + "/education-elearning"} element={<EducationElearning/>}/>
-				<Route path={process.env.PUBLIC_URL + "/retail-ecommerce"} element={<RetailEcommerce />}/>
-				
+					<Route path={process.env.PUBLIC_URL + "/hire-dot-net-developers"} element={<DotNetDevelopers/>}/>
 
-			</Routes>
-		</ScrollToTop>
-    </Router>
-  )
+					<Route path={process.env.PUBLIC_URL + "/hire-react-native-developers"} element={<ReactNativeDevelopers/>}/>
+
+					<Route path={process.env.PUBLIC_URL + "/programmatic-advertising"} element={<ProgrammaticSpecialist/>}/>
+					<Route path={process.env.PUBLIC_URL + "/education-elearning"} element={<EducationElearning/>}/>
+					<Route path={process.env.PUBLIC_URL + "/retail-ecommerce"} element={<RetailEcommerce />}/>
+					
+
+				</Routes>
+			</ScrollToTop>
+		</Router>
+	)
 }
 
 export default App;
--- src/store.js
+++ src/store.js
@@ -1,5 +1,4 @@
-
-import { createStore, combineReducers, applyMiddleware } from "redux";
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { formReducer } from "./reducers/formReducer";
